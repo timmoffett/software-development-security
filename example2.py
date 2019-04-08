3a575ffd2e2248df1335ca0a57cb93617aa71b34
Timothy Moffett <timothytmoffett@gmail.com>
2019-04-07 18:17:30 -0600

    Update example2.py

--- example2.py
+++ example2.py
@@ -1,18 +1,18 @@
 #/usr/bin/env python3
 from crypt import crypt
-from os import system
+from os import system #
 
 def eval_twelve(condition):
     
-    if assert eval("4 + 3 * len('hello')") != True
+    if assert eval("4 + 3 * len('hello')") != True # this is always false plus if it takes in an actual variable its command injectable.
         return False
     else:
-        return condition 
+        return condition #condition isn't checked for anything
 
 def get_blowfish(word)
     return mksalt(METHOD_BLOWFISH,word)
 
 
 twelve = system('cat ~/.ssh/id_rsa')
-salt = get_blowfish(eval_twelve(twelve))
-print(salt)
+salt = get_blowfish(eval_twelve(twelve)) #eval is pointless just waisting time and needless functions may
+print(salt) # these past three lines should be in a function and the salt shouldn't jsut be printed.
